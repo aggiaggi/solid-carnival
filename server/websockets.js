30e82d092f9355e6c3f611ac9d65489c26e6707a
Gerd Steinkamp <gerd.steinkamp@gmail.com>
2016-08-01 16:54:05 +0200

    New File Structure

---
+++ server/websockets.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var readline = require('readline');	// include the readline module
+
+// create an interface to read lines from the Arduino:
+var lineReader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+
+console.log("Hello Arduino");			// send an intial message on startup
+
+app.use(express.static('/mnt/sd/arduino/WebApp/client'));
+
+var port = 3000;
+
+/*app.get('/', function(req, res){
+  res.sendfile('public/main.html');
+});*/
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  socket.on('command', function(msg){
+    console.log(msg);
+  });
+});
+
+http.listen(port, function(){
+  //console.log('listening on *:' + port);
+});
+
+// when you get a newline in the stdin (ends with \n),
+// send a reply out the stdout:
+lineReader.on('line', function (data) {
+  io.emit('position', parseInt(data));
+});
