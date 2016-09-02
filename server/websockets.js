751c61940f5943d15a0bb39ba0fdfe0891d5386c
Gerd Steinkamp <gerd.steinkamp@gmail.com>
2016-09-02 11:22:10 +0200

    Changed to serial interface. Bridge library dependency removed

--- server/websockets.js
+++ server/websockets.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var readline = require('readline');	// include the readline module
 
-// create an interface to read lines from the Arduino:
-var lineReader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
-
-
-console.log("Hello Arduino");			// send an intial message on startup
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("/dev/ttySAMD", {
+    baudrate: 400000,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+}, false);
 
 app.use(express.static('/webapp/client'));
 
@@ -31,6 +28,7 @@ io.on('connection', function(socket){
   
   socket.on('command', function(msg){
     console.log(msg);
+	serialPort.write(msg);
   });
 });
 
@@ -38,8 +36,25 @@ http.listen(port, function(){
   //console.log('listening on *:' + port);
 });
 
-// when you get a newline in the stdin (ends with \n),
-// send a reply out the stdout:
-lineReader.on('line', function (data) {
-  io.emit('position', data);
+
+serialPort.on("open", function () {
+    console.log('open');
+	serialPort.write("Hello Arduino");			// send an intial message on startup
+    serialPort.on('data', function(data) {
+		try {
+			var obj = JSON.parse(data);
+			var str = JSON.stringify(obj);
+        	//console.log('data received: ' + str);
+			io.emit('position', str);
+		}
+		catch(err){
+			console.log("Error while parsing JSON data!")
+		}
+    });
 });
+
+serialPort.on('error', function(err) {
+    console.log('error: '+err);
+});
+
+serialPort.open();
