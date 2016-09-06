16ac2c58a850712c86d4ae641a4a33ca78e16bdf
Gerd Steinkamp <gerd.steinkamp@gmail.com>
2016-09-06 12:57:16 +0200

    BUGFIX: Multiple reconnects on serial port now possible.

--- server/websockets.js
+++ server/websockets.js
@@ -7,14 +7,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort(serial_port, {
+var serialPort;
+/*= new SerialPort(serial_port, {
     baudrate: 400000,
     dataBits: 8,
     parity: 'none',
     stopBits: 1,
     flowControl: false
 }, false); //autoOpen = false
-
+*/
 app.use(express.static('/webapp/client'));
 
 
@@ -60,34 +61,58 @@ io.on('error', function(err){
 //----------------------
 
 //Open serial port
-serialPort.open(function (err){
-	if (err) {
-		console.log("Serial port" + serial_port + " failed to open: " + err);
-	}
-});
+openSerialPort(20,2000);
 
-//Handle serial port "open" event after successful connection
-serialPort.on("open", function () {
-    console.log("Serial port" + serial_port + " open");
+//Recusrsive function that tries to open serial port
+//attempts: number of retries
+//waittime: delay between attempts 
+function openSerialPort(attempts, waittime) {
+	console.log(attempts + ' attempts remaining');
+	console.log('Waiting for ' + waittime + ' ms');
+	sleep(waittime);
+	console.log('Trying to reopen Serial port');
 	
-	// send an intial message to MCU on startup
-	serialPort.write("Websocket server connected via the serial port " + serial_port);			
+	serialPort = new SerialPort(serial_port, {
+	    baudrate: 400000,
+	    dataBits: 8,
+	    parity: 'none',
+	    stopBits: 1,
+	    flowControl: false
+	}, false);
 	
+	serialPort.open(function (err){
+		if (err) {
+			console.log("Serial port failed to open: " + err);
+			openSerialPort(attempts-1, waittime);
+		}
+	});
+	
+	//Handle serial port "open" event after successful connection
+	serialPort.on("open", function () {
+	    console.log("Serial port" + serial_port + " open");
+	
+		// send an intial message to MCU on startup
+		serialPort.write("Websocket server connected via the serial port " + serial_port);			
+	
+	
+	});
+
 	//Handle incoming data
-    serialPort.on('data', function(data) {
+	serialPort.on('data', function(data) {
 		try {
 			//Validate received data by parsing into JSON object
 			var obj = JSON.parse(data);
 			//JSON object is transferred back into string representation
 			var str = JSON.stringify(obj);
-        	//console.log('data received: ' + str);
+	    	//console.log('data received: ' + str);
 			io.emit('position', str);
 		}
 		catch(err){
 			//Log error if received data is not a valid JSON string
 			console.log("Error while parsing JSON data!")
 		}
-    });
+	});
+
 	//Before serial port is closed the "disconnect" event is emitted
 	serialPort.on('disconnect', function(err) {
 	    console.log('Serial port disconnected: ' + err);
@@ -96,39 +121,18 @@ serialPort.on("open", function () {
 	//Handle the serial port "close" event
 	serialPort.on('close', function() {
 	    console.log('Serial port closed');
-		reopenSerialPort(20, 2000);
+		openSerialPort(20, 2000);
 	});
-});
 
-//Handle serial port "error" event
-serialPort.on('error', function(err) {
-    console.log('Serial port error: '+err);
-});
-
-
-
-
-
-//Recusrsive function that tries to reopen serial port
-//attempts: number of retries
-//waittime: delay between attempts 
-function reopenSerialPort(attempts, waittime) {
-	console.log(attempts + ' attempts remaining');
-	console.log('Waiting for ' + waittime + ' ms');
-	sleep(waittime);
-	console.log('Trying to reopen Serial port');
-	serialPort.open(function (err){
-		if (err) {
-			console.log("Serial port failed to reopen: " + err);
-			reopenSerialPort(attempts-1, waittime);
-		} else {
-			console.log("Success! Serial port reopened.");
-		}
+	//Handle serial port "error" event
+	serialPort.on('error', function(err) {
+	    console.log('Serial port error: '+err);
 	});
+	
+	//Helper function to realize sleep(milliseconds) function
+	function sleep(delay) {
+		var start = new Date().getTime();
+		while (new Date().getTime() < start + delay);
+	}
 }
 
-//Helper function to realize sleep(milliseconds) function
-function sleep(delay) {
-	var start = new Date().getTime();
-	while (new Date().getTime() < start + delay);
-}
