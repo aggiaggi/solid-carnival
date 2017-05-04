f3fa32777c12a3847c4e605773d39bc3dde32cec
Gerd Steinkamp <gerd.steinkamp@gmail.com>
2017-05-04 14:54:18 +0200

    Added member variables

--- client/src/app/motor/motor.component.ts
+++ client/src/app/motor/motor.component.ts
@@ -8,17 +8,59 @@ export class  MotorComponent {
   model: string;
   manufacturer: string;
   size: string;
-  ratedVoltage: number;
-  ratedCurrent: number;
-  phaseResistance: number;
-  phaseInductance: number;
-  holdingTorque: number;
-  rotorInertia: number;
-  stepAngle: number;
-  electricalConstant: number;
-  vBus: number;
+  resistance: number; //phase resistance in Ohm
+  inductance: number; //phase inductance in mH
+  torque: number; //holding torque in Ncm
+  inertia: number; //rotor inertia
+  stepAngle: number; //step angle
+  electricalConstant: number; //electrical constant
+  private _kval:number;
   
-  kval(): number {
-  	return this.ratedVoltage * this.ratedCurrent / vBus;
+  constructor(public: _ratedVoltage: number,
+  						public: _ratedCurrent: mumber,
+  						public: _vsupply: number) {. //TODO: vbus to be injected
+  	this.calculateKval();
+  }
+  
+  protected calculateKval(): number {
+  	return this.ratedVoltage * this.ratedCurrent / this.vsupply;
+  }
+  
+  set ratedVoltage(v) {
+  	this._ratedVoltage = v;
+  	this.calculateKval();
+  }
+  
+  get ratedVoltage() {
+  	return this._ratedVoltage;
+  }
+  
+  set ratedCurrent(c) {
+  	this._ratedCurrent = c;
+  	this.calculateKval();
+  }
+  
+  get ratedCurrent() {
+  	return this._ratedCurrent;
+  }
+  
+  set vsupply(v) {
+  	this._vsupply = v;
+  	this.calculateKval();
+  }
+  
+  get vsupply(v) {
+  	return this._vsupply;
+  }
+  
+  get kval() {
+  	return this._kval;
+  }
+  
+  toString(): string {
+  	return `Motor ${this.model}, 
+  		${this.ratedCurrent}A, 
+  		${this.ratedVoltage}V
+  		`;
   }
 }
