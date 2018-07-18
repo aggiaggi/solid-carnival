62319300369aeffe27a0ca5393d5f3eb37bea90e
Gerd Steinkamp <gerd.steinkamp@gmail.com>
2018-07-18 16:23:32 +0200

    Loading axis config data from jsonDB server

--- client/src/app/axes/axes.component.ts
+++ client/src/app/axes/axes.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Inject } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import "rxjs/add/operator/map";
 import { AxisConfigService } from '../services/axis-config.service';
 import { Axis } from '../axis/axis';
 import { AxisConfig } from '../models/model-interfaces';
@@ -9,29 +12,47 @@ import { AxisConfig } from '../models/model-interfaces';
     selector: 'axes',
     templateUrl: './axes.component.html',
 })
-export class AxesComponent {
-    axes: Array<Axis> = [];
-
-    debug: boolean;
+export class AxesComponent implements OnInit {
+    axisConfigList: AxisConfig[];   //Axis config data (JSON)
+    axes: Array<Axis> = [];         //Array with all axis objects
+    debug: boolean;                 //flag to indicate if debug information shall be displayed in html view
     
     constructor(@Inject('DEBUG') debug,
-        private axisConfigService: AxisConfigService) { 
+        private axisConfigService: AxisConfigService,
+        private http: Http) { 
         this.debug = debug;  
     }
 
     ngOnInit(): void {
-        //Get axis config data from AxisConfigService 
-        //and create all axes
-        let axisConf: AxisConfig;
-        let axis: Axis;
-        let numberOfAxes = this.axisConfigService.getNumberOfAxes();
-        let i:number;
-        
-        for (i=1; i<=numberOfAxes; i++){
-            axisConf = this.axisConfigService.getAxisConfig(i);
-            axis = Axis.create(axisConf);
-            this.axes.push(axis);
-         };
+        //Get axis config data from jsonDB server
+        this.http.get('http://localhost:3001/axes')
+            //for each response get JSON object
+            .map((response: Response) => response.json())
+            //create axis object from each axisConfig JSON data block
+            .map((axisConfigListAsJson: any) => {
+                return axisConfigListAsJson.map((data) => {
+                    let axis:Axis = Axis.create(data);
+                    console.log("Axis: " + axis.toString());
+                    return Axis.create(data);
+                });
+            })
+            //subscribe to Observable to get notified when new HTTP data arrives
+            .subscribe(axes => {
+                this.axes = axes;
+            },
+            //Error Handling
+            (error: Response) => {
+              switch (error.status) {
+                case 404:
+                  console.log('Ressource not found', error);
+                  break;
+                case 500:
+                  console.log('Axis config data could not be loaded', error);
+                  break;
+                default:
+                  console.log('Something went wrong', error);
+              }
+            });
     }
 
     toString(): string {
